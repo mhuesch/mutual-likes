ca393fc3bce98c6147e6c989858866766ebc8f39
Michael Hueschen <michaelhueschen@gmail.com>
2013-12-20 15:51:30 -1000

    Switch to Postgres

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           scope: 'name,email,user_likes,user_friends,friends_likes'
+           scope: 'email,user_likes,user_friends,friends_likes'
 end
