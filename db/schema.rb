6dfd93aac98bd8d9656fc2c5960d45a7d82d8c82
Michael Hueschen <michaelhueschen@gmail.com>
2013-12-26 23:10:55 -0800

    Get rid of Users & Authorizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221060135) do
+ActiveRecord::Schema.define(version: 20131227070703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authorizations", force: true do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "uid"
-    t.string   "token"
-  end
 
 end
